cf209c471d09cc7a218f6ef2e19c384860cb0d2b
Michael Yarichuk <michael.yarichuk@sap.com>
2021-06-20 10:23:23 +0300

    encoder tests

---
+++ RLE.CSharp.Tests/Basics.cs
@@ -0,0 +1,24 @@
+using System;
+using Xunit;
+
+namespace RLE.CSharp.Tests
+{
+    public class Basics
+    {
+        [Fact]
+        public void Can_encode_single_letter() => 
+            Assert.Equal("1a", Encoder.Process("a"));
+
+        [Fact]
+        public void Can_encode_single_letter_group() => 
+            Assert.Equal("5a", Encoder.Process("aaaaa"));
+
+        [Fact]
+        public void Can_encode_multiple_letter_group1() => 
+            Assert.Equal("3c5a2d4t", Encoder.Process("cccaaaaaddtttt"));
+
+        [Fact]
+        public void Can_encode_multiple_letter_group2() => 
+            Assert.Equal("1x1y1z3c5a2d4t3a4b1a1b1a1b", Encoder.Process("xyzcccaaaaaddttttaaabbbbabab"));
+    }
+}
--- RLE.CSharp/Encoder.cs
+++ RLE.CSharp/Encoder.cs
@@ -1,9 +1,24 @@
 ﻿using System;
+using System.Linq;
 
 namespace RLE.CSharp
 {
-    public class Encoder
+    public static class Encoder
     {
-        
+        /// <summary>
+        /// Runtime-Length Encoder is an algorithm to compress data
+        /// by eliminating repetitions. 
+        /// It was used in old image formats sometimes, before JPG was invented.
+        /// </summary>
+        /// <param name="data">Series of lowercase letters, for example 'bbaaacccdee'</param>
+        /// <returns>compress data, so 'bbaaacccdee' will turn to '2b3a3c1d2e'</returns>
+        public static string Process(string data)
+        {
+            if(!data.All(c => char.IsLetter(c) && char.IsLower(c)))
+                throw new ArgumentException("Invalid input");
+            string encodedData = data;
+
+            return encodedData;
+        }
     }
 }
